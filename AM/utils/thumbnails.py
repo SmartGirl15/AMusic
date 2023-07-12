52227844e45aba899e596b6cf04f025dd4162c4e
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-12 17:19:16 +0530

    Update thumbnails.py

--- AM/utils/thumbnails.py
+++ AM/utils/thumbnails.py
@@ -72,7 +72,7 @@ async def gen_thumb(videoid, user_id):
             hehe = await app.get_profile_photos(app.id)
             wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
         xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
+        a = Image.new('L', [500, 500], 0)
         b = ImageDraw.Draw(a)
         b.pieslice([(0, 0), (400,400)], 0, 270, fill = 255, outline = "white")
         c = np.array(xy)
