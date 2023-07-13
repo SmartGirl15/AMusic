9f2c27b9aeb36c09277a34333d06b514a95d79b9
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-13 11:52:23 +0530

    Update thumbnails.py

--- AM/utils/thumbnails.py
+++ AM/utils/thumbnails.py
@@ -110,7 +110,7 @@ async def gen_thumb(videoid, user_id):
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
         logo.thumbnail((600, 600), Image.LANCZOS)
-        width = int((1280 - 365) / 2)
+        width = int((1280 - 600) / 2)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width, 138), mask=logo)
         background.paste(x, (1010, 427), mask=x)
