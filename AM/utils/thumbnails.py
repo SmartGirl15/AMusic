46f411e6aeca67d44d344ad657272f20b94e668a
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-14 09:38:43 +0530

    Update thumbnails.py

--- AM/utils/thumbnails.py
+++ AM/utils/thumbnails.py
@@ -25,7 +25,7 @@ def changeImageSize(maxWidth, maxHeight, image):
 def add_corners(im):
     bigsize = (im.size[0] * 3, im.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
-    size = [(0,0), bigsize]
+    size = [(0,100), bigsize]
     ImageDraw.Draw(mask).rectangle(size, fill=255)
     mask = mask.resize(im.size, Image.LANCZOS)
     mask = ImageChops.darker(mask, im.split()[-1])
