4a4105cbdefd4a4fe9006ce879046fdf3273edc8
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-17 15:53:09 +0530

    Update thumbnails.py

--- AM/utils/thumbnails.py
+++ AM/utils/thumbnails.py
@@ -67,7 +67,7 @@ async def gen_thumb(videoid, user_id):
                     await f.close()
 
         try:
-            wxyz = await app.get_profile_photos(message.chat.id)
+            wxyz = await app.get_profile_photos(chat_id)
             wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{chat_id}.jpg')
         except:
             hehe = await app.get_profile_photos(app.id)
